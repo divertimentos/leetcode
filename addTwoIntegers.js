fca6734ccfd83801c18937c4b6e1ba2025fb6fe3
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-06-12 08:18:28 -0300

    Creates a Python version of the exercise

--- addTwoIntegers.js
+++ addTwoIntegers.js
@@ -5,5 +5,5 @@
  */
 
 const sum = (num1, num2) => {
-  return num1 + num2
-}
+  return num1 + num2;
+};
---
+++ addTwoIntegers.py
@@ -0,0 +1,10 @@
+import os
+
+
+def sum(num1, num2):
+    return num1 + num2
+
+
+os.system("clear")
+
+print(sum(1, 2))
