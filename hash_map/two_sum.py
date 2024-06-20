4a35b12b57ca2d0ae509b6e5c2e6cc0d0de04399
Guilherme Teixeira <24235344+ggteixeira@users.noreply.github.com>
2024-06-19 21:27:42 -0300

    I solved this exercise 3 days ago :clown:

--- hash_map/two_sum.py
+++ hash_map/two_sum.py
@@ -12,10 +12,12 @@ such that they add up to a specific target.
 
 def two_sum(nums: List[int], target: int) -> List[int]:
     num_to_index = {}
+
     for i in range(len(nums)):
         if target - nums[i] in num_to_index:
             return [num_to_index[target - nums[i]], i]
         num_to_index[nums[i]] = i
+
     return []
 
 
